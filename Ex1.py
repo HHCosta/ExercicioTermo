82e1e23ac3e567d749e4f966d9dc6d8891f47a65
HHCosta <128651493+HHCosta@users.noreply.github.com>
2026-03-23 20:11:41 -0300

    Add files via upload

---
+++ Ex1.py
@@ -0,0 +1,24 @@
+# Conversão de unidades
+
+print("Conversor de medidas\n")
+a = 1
+while a ==1:
+
+    options = int(input("Escolha a conversão: \n1)N⮕kgf \n2)kPa⮕bar \n3)PSI⮕bar \n4)cm²⮕m²\n"))
+    valor = int(input("Coloque o valor da unidade: "))
+    if options == 1:
+        resultado = valor/9.807
+        print(valor , "N = "+"% .4f" % resultado + "m²")
+    elif options == 2:
+        resultado = valor/100
+        print(valor , "kPa = "+"% .4f" % resultado + "m²")
+    elif options == 3:
+        resultado = valor/14.504
+        print(valor , "PSI = "+"% .4f" % resultado + "m²")
+    elif options == 4:
+        resultado = valor/10000
+        print(valor,"cm² = % .4f" % resultado + "m²")
+    else:
+        print("Conversão inválida")
+
+    a = int(input("Deseja converter mais alguma medida? Digite 1 se sim.\n"))
